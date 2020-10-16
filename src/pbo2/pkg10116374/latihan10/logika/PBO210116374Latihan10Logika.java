ad53af11697a4221f501cbe5490e82be03e7a104
mortdecai40 <rizkinabil.999@gmail.com>
2020-10-16 14:14:25 +0700

    Program logika

---
+++ src/pbo2/pkg10116374/latihan10/logika/PBO210116374Latihan10Logika.java
@@ -0,0 +1,21 @@
+package pbo2.pkg10116374.latihan10.logika;
+
+/** @author
+ * NAMA                 : TEUKU RIZKI NABIL
+ * KELAS                : PBO2
+ * NIM                  : 10116374
+ * Deskripsi Program    : Program untuk memasukan logika
+ */
+public class PBO210116374Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 20;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
